24be465e38ee404c50f468234904f009e48b5aaf
Tentuso <tenutso@yahoo.com>
2021-12-15 16:53:14 -0500

    Update import_session.js

--- src/tools/import_session.js
+++ src/tools/import_session.js
@@ -1,8 +1,6 @@
-//require("dotenv").config({ path: "../../.env" });
+require("dotenv").config({ path: "../../.env" });
 const { MongoClient } = require("mongodb");
-const client = new MongoClient(
-  "mongodb+srv://doadmin:E6PI43h091oN5Z7M@db-mongodb-tor1-77347-2d3642c8.mongo.ondigitalocean.com/appointific?authSource=admin&tls=true&tlsCAFile=../ca-certificate.crt"
-);
+const client = new MongoClient(process.env.DATABASE_URL);
 const csv = require("csvtojson");
 const eventId = "dxS1qT1YIBZ5sjB95Q0k";
 const csvFilePath = "./sessions_import_" + eventId + ".csv";
